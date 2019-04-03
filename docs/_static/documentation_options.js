a6d11e3adad590e8a1a7bf22c7760ab636246495
大鼻子 <wohugb@qq.com>
2019-04-04 00:20:15 +0800

    翻译配置

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.1.0+/5654598',
+    VERSION: '2.1.0+/efa8fcc27',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
