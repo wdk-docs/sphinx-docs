ec574630c02e83edceec89269126a1e85264dc76
大鼻子 <wohugb@qq.com>
2019-04-08 18:53:41 +0800

    整理

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.1.0+/41035e8f9',
+    VERSION: '2.1.0+/c6c93cfdb',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
